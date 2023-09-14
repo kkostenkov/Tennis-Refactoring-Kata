ef466f82ce23db77672b6fe5bc76058712ec84d6
Kirill Kostenkov <kiril.kostenkov@tripledotfortune.com>
2023-09-14 20:46:50 +0200

    wip

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -23,83 +23,48 @@ namespace Tennis
 
         public string GetScore()
         {
-            var p1res = "";
-            var p2res = "";
             var score = "";
-            if (p1point == p2point && p1point < 3)
-            {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
+            var p1res = GetPointsName(p1point);
+            var p2res = GetPointsName(p2point);
+            
+            if (p1point == p2point && p1point < 3) {
+                score = GetPointsName(p1point);
                 score += "-All";
             }
-            if (p1point == p2point && p1point > 2)
+            if (p1point == p2point && p1point > 2) {
                 score = "Deuce";
+            }
 
-            if (p1point > 0 && p2point == 0)
-            {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
+            if (p1point > 0 && p2point == 0) {
                 score = p1res + "-" + p2res;
             }
-            if (p2point > 0 && p1point == 0)
-            {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
+            if (p2point > 0 && p1point == 0) {
                 score = p1res + "-" + p2res;
             }
 
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
+            if (p1point > p2point && p1point < 4) {
                 score = p1res + "-" + p2res;
             }
-            if (p2point > p1point && p2point < 4)
-            {
-                p1res = GetPointsName(p1point);
-                p2res = GetPointsName(p2point);
+            if (p2point > p1point && p2point < 4) {
                 score = p1res + "-" + p2res;
             }
 
-            if (p1point > p2point && p2point >= 3)
-            {
+            if (p1point > p2point && p2point >= 3) {
                 score = "Advantage player1";
             }
 
-            if (p2point > p1point && p1point >= 3)
-            {
+            if (p2point > p1point && p1point >= 3) {
                 score = "Advantage player2";
             }
 
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for player1";
+            if (p1point >= 4 && p2point >= 0){
+                if (p1point - p2point >= 2) {
+                    return "Win for player1";    
+                }
             }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for player2";
+            
+            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2) {
+                return "Win for player2";
             }
             return score;
         }
